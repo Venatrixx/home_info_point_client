2c7e93204168096caf5c728bf2d9b0664a141924
Venatrix <venatrix945@gmail.com>
2025-11-15 19:46:38 +0100

    bugfix: Sozialkunde not importing

--- lib/src/interpreter.dart
+++ lib/src/interpreter.dart
@@ -10,11 +10,18 @@ Map interpretData(String htmlString) {
 
   data['name'] = tables01[0].children[0].children[0].children[1].text;
   final classString = tables01[0].children[0].children[1].children[1].text;
-  final truncClassString = classString.substring(0, classString.indexOf(RegExp('[^0-9A-Z]')));
+  final truncClassString = classString.substring(
+    0,
+    classString.indexOf(RegExp('[^0-9A-Z]')),
+  );
   data['class'] = truncClassString;
-  data['level'] = int.tryParse(truncClassString.substring(0, truncClassString.indexOf(RegExp('[^0-9]'))));
+  data['level'] = int.tryParse(
+    truncClassString.substring(0, truncClassString.indexOf(RegExp('[^0-9]'))),
+  );
 
-  RegExp subjectTitlePattern = RegExp(r'[a-zA-ZäöüÄÖÜß\(\)]+[-]+[a-zA-ZäöüÄÖÜß]+\(+');
+  RegExp subjectTitlePattern = RegExp(
+    r'[a-zA-ZäöüÄÖÜß\(\)]+[-]+[a-zA-ZäöüÄÖÜß/&]+\(+',
+  );
   final htmlData = document.querySelectorAll('h3, h2, table[class="t02"]');
 
   List subjects = [];
@@ -22,7 +29,8 @@ Map interpretData(String htmlString) {
   // loop through headlines and tables
   for (int i = 0; i < htmlData.length; i++) {
     // check if element is a headline type h3
-    if (htmlData[i].localName == 'h3' && RegExp(r'[a-zA-Z]+').hasMatch(htmlData[i].text)) {
+    if (htmlData[i].localName == 'h3' &&
+        RegExp(r'[a-zA-Z]+').hasMatch(htmlData[i].text)) {
       if (htmlData[i].text == "Fehltage") {
         // read missing days
         List missingDays = [];
@@ -53,13 +61,21 @@ Map interpretData(String htmlString) {
         // read summary of missing days and hours
         final rows = htmlData[i + 1].children[0].children;
         data['totalMissingDays'] = int.tryParse(rows[0].children[1].text);
-        data['totalUnexcusedMissingDays'] = int.tryParse(rows[1].children[1].text);
+        data['totalUnexcusedMissingDays'] = int.tryParse(
+          rows[1].children[1].text,
+        );
 
         data['totalMissingHours'] = int.tryParse(
-          rows[2].children[1].text.substring(0, rows[2].children[1].text.indexOf(RegExp(r'[^0-9]'))),
+          rows[2].children[1].text.substring(
+            0,
+            rows[2].children[1].text.indexOf(RegExp(r'[^0-9]')),
+          ),
         );
         data['totalUnexcusedMissingHours'] = int.tryParse(
-          rows[3].children[1].text.substring(0, rows[3].children[1].text.indexOf(RegExp('[^0-9]'))),
+          rows[3].children[1].text.substring(
+            0,
+            rows[3].children[1].text.indexOf(RegExp('[^0-9]')),
+          ),
         );
       } else if (htmlData[i].text == "Vergessene Hausaufgaben") {
         // read forgotten homework
@@ -98,7 +114,10 @@ Map interpretData(String htmlString) {
     }
 
     // skip if element is not the headline of a subject table
-    if (htmlData[i].localName != 'h3' || !subjectTitlePattern.hasMatch(htmlData[i].text.replaceAll(RegExp(r' '), ''))) {
+    if (htmlData[i].localName != 'h3' ||
+        !subjectTitlePattern.hasMatch(
+          htmlData[i].text.replaceAll(RegExp(r' '), ''),
+        )) {
       continue;
     }
 
@@ -106,8 +125,12 @@ Map interpretData(String htmlString) {
 
     final String subjectTitle = htmlData[i].text;
 
-    subject['name'] = subjectTitle.substring(subjectTitle.indexOf('-') + 1, subjectTitle.lastIndexOf('(')).trim();
-    subject['abbr'] = subjectTitle.substring(0, subjectTitle.indexOf('-')).trim();
+    subject['name'] = subjectTitle
+        .substring(subjectTitle.indexOf('-') + 1, subjectTitle.lastIndexOf('('))
+        .trim();
+    subject['abbr'] = subjectTitle
+        .substring(0, subjectTitle.indexOf('-'))
+        .trim();
 
     subject['finalGradeSem1'] = null;
     subject['finalGradeSem2'] = null;
@@ -136,8 +159,17 @@ Map interpretData(String htmlString) {
       grade['date'] = row.children[0].text;
       grade['value'] = row.children[1].text;
       grade['comment'] = row.children[2].text;
-      grade['isExam'] = (['klausur', 'klassenarbeit', 'ka'].any(row.children[3].text.toLowerCase().contains));
-      grade['semester'] = int.parse(row.children[4].text.substring(0, row.children[4].text.indexOf(RegExp('[^0-9]'))));
+      grade['isExam'] = ([
+        'klausur',
+        'klassenarbeit',
+        'ka',
+      ].any(row.children[3].text.toLowerCase().contains));
+      grade['semester'] = int.parse(
+        row.children[4].text.substring(
+          0,
+          row.children[4].text.indexOf(RegExp('[^0-9]')),
+        ),
+      );
 
       grades.add(grade);
     }
